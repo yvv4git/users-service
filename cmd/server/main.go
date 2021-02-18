5e37df553416752a897f48fd4d844705265d7c91
Владимир Елисеев <ve@kvado.ru>
2021-02-18 11:39:22 +0300

    Modify server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,37 +3,68 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
-	/* "github.com/yvv4git/users-service/internal/api" */
+	"github.com/yvv4git/users-service/internal/api"
 	"github.com/yvv4git/users-service/internal/config"
-	/* 	"github.com/yvv4git/users-service/internal/servers"
-	   	"google.golang.org/grpc" */)
+	"github.com/yvv4git/users-service/internal/repository/sqlite"
+	"github.com/yvv4git/users-service/internal/servers"
+	"github.com/yvv4git/users-service/internal/services"
+	"google.golang.org/grpc"
+)
 
 const configPath = "config/main"
 
 func main() {
-	fmt.Println("Server")
+	log.Println("Server start")
 
+	// Init config.
 	cfg, err := config.Init(configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//config.setUpByViper()
-	log.Println(cfg)
+
+	// Init db connection.
+	db, err := sqlite.NewSqliteDB(cfg.DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Init repository, service.
+	usersRepo := sqlite.NewUsersRepository(db)
+	usersService := services.NewUsersService(usersRepo)
+	usersSrv := servers.NewUsersServer(usersService)
+	log.Println(usersSrv)
+
+	// Graceful shutdown.
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	// Create and register grpc server.
-	/* 	grpcServer := grpc.NewServer()
-	   	usersServer := servers.NewUsersServer()
-	   	api.RegisterUsersServer(grpcServer, usersServer)
+	grpcServer := grpc.NewServer()
+	api.RegisterUsersServer(grpcServer, usersSrv)
 
-	   	// Bind port.
-	   	tcpListner, err := net.Listen("tcp", ":1234")
-	   	if err != nil {
-	   		log.Fatal(err)
-	   	}
+	// Bind port.
+	address := fmt.Sprintf("%s:%s", "", strconv.Itoa(cfg.Server.Port))
+	tcpListner, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	   	// Start server.
-	   	if err = grpcServer.Serve(tcpListner); err != nil {
-	   		log.Fatal(err)
-	   	} */
+	// Run grpc server.
+	go func() {
+		if err = grpcServer.Serve(tcpListner); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Gracefull exit.
+	<-exit
+	log.Println("Stopping app...")
+	grpcServer.Stop()
+	db.Close()
 }
--- internal/servers/users.go
+++ internal/servers/users.go
@@ -2,6 +2,7 @@ package servers
 
 import (
 	"context"
+	"log"
 
 	"github.com/yvv4git/users-service/internal/api"
 	"github.com/yvv4git/users-service/internal/services"
@@ -11,31 +12,36 @@ import (
 
 // Users ...
 type Users struct {
-	userService services.UsersService
+	userService *services.UsersService
 }
 
 // Create ...
-func (*Users) Create(context.Context, *api.CreateRequest) (*api.CreateResponse, error) {
+func (u *Users) Create(context.Context, *api.CreateRequest) (*api.CreateResponse, error) {
+	//log.Println(u.userService)
+	log.Println("Create")
 	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
 }
 
 // Read ...
-func (*Users) Read(context.Context, *api.ReadRequest) (*api.ReadResponse, error) {
+func (u *Users) Read(context.Context, *api.ReadRequest) (*api.ReadResponse, error) {
+	log.Println("Read")
 	return nil, status.Errorf(codes.Unimplemented, "method Read not implemented")
 }
 
 // Update ...
-func (*Users) Update(context.Context, *api.UpdateRequest) (*api.UpdateResponse, error) {
+func (u *Users) Update(context.Context, *api.UpdateRequest) (*api.UpdateResponse, error) {
+	log.Println("Update")
 	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
 }
 
 // Del ...
-func (*Users) Del(context.Context, *api.DelRequest) (*api.DelResponse, error) {
+func (u *Users) Del(context.Context, *api.DelRequest) (*api.DelResponse, error) {
+	log.Println("Del")
 	return nil, status.Errorf(codes.Unimplemented, "method Del not implemented")
 }
 
 // NewUsersServer is used as constructor for users server protocol.
-func NewUsersServer(usersService services.UsersService) *Users {
+func NewUsersServer(usersService *services.UsersService) *Users {
 	return &Users{
 		userService: usersService,
 	}
